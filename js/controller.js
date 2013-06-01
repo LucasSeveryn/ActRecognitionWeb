a008d140e4c044bf425cd4a0391b139cb3a38410
LucasSeveryn <moa.1991@gmail.com>
2013-06-01 20:30:49 +0100

    instead of ajax now is used

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-function Ctrl($scope){
+function Ctrl($scope, $http){
 
   $scope.loginUsername = "";
   $scope.loginPassword = "";
@@ -9,23 +9,8 @@ function Ctrl($scope){
   $scope.apiKey = "Ix7evhXTw3uwk1gDHCvzz-uMNEhOy8ZN";
 
   $scope.logIn = function() {
-    // alert("called");
-    $.ajax( {
-     url: "https://api.mongolab.com/api/1/databases/activity_recognition/collections/users?q={%22username%22:%20%22" + $scope.loginUsername + "%22,%22password%22:%22" + $scope.loginPassword + "%22}&apiKey=" + $scope.apiKey,
-     type: "GET",
-     // dataType: 'json',
-     contentType: "application/json",
-     success: function(data){
-       if(data.length==1){
-         var user = data[0];
-         $scope.loggedIn=true;
-         $scope.userName=user.name;
-         $scope.userSurname=user.surname;
-         $scope.userId=user.userid;
-       }
-     }
-   }
-   );
+    var url = "https://api.mongolab.com/api/1/databases/activity_recognition/collections/users?q={%22username%22:%20%22" + $scope.loginUsername + "%22,%22password%22:%22" + $scope.loginPassword + "%22}&apiKey=" + $scope.apiKey;
+    $http.get(url).success(function(data, status, headers, config) { if(data.length==1){var user = data[0]; $scope.loggedIn=true; $scope.userName=user.name; $scope.userSurname=user.surname; $scope.userId=user.userid;}});
   };
 
 
