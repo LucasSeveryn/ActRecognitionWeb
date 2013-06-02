fa1b1d6bc42608282f1432a66855b2c0f6c38e2f
LucasSeveryn <moa.1991@gmail.com>
2013-06-02 21:54:52 +0100

    daily type distribution plotted, datepicker added

--- appjs/directives.js
+++ appjs/directives.js
@@ -3,10 +3,34 @@ App.directive('chart', function(){
         restrict: 'E',
         link: function(scope, elem, attrs){
             var data = scope[attrs.ngModel];
+            var r = Raphael("pie");
+            var pie = null;
 
             scope.$watch('typeDistribution', function(v){
-                    var r = Raphael("pie");
-                    r.piechart(120, 120, 90, [1,2,3]);
+              if(pie){
+                   r.clear();
+                   pie.clear();
+               }
+
+              r = Raphael("pie"),
+              pie = r.piechart(120, 120, 90, v, { legend: [""]});
+              pie.hover(function () {
+                  this.sector.stop();
+                  this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+                  if (this.label) {
+                      this.label[0].stop();
+                      this.label[0].attr({ r: 7.5 });
+                      this.label[1].attr({ "font-weight": 800 });
+                  }
+              }, function () {
+                  this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+
+                  if (this.label) {
+                      this.label[0].animate({ r: 5 }, 500, "bounce");
+                      this.label[1].attr({ "font-weight": 400 });
+                  }
+              });
             });
         }
     };
