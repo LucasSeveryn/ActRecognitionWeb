4d90589f6f75251feca47c2f12bd67f3fd6aa106
LucasSeveryn <moa.1991@gmail.com>
2013-06-03 01:37:46 +0100

    RealTime: start/stop of every 3 sec function invocation

--- appjs/controllers.js
+++ appjs/controllers.js
@@ -1,69 +1,4 @@
-App.controller('Ctrl', function ($scope, $http){
-
-  /*
-
-  DATE MANIPULATION UTILITY FUNCTIONS
-
-  */
-  var dates = {
-      convert:function(d) {
-          // Converts the date in d to a date-object. The input can be:
-          //   a date object: returned without modification
-          //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
-          //   a number     : Interpreted as number of milliseconds
-          //                  since 1 Jan 1970 (a timestamp)
-          //   a string     : Any format supported by the javascript engine, like
-          //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
-          //  an object     : Interpreted as an object with year, month and date
-          //                  attributes.  **NOTE** month is 0-11.
-          return (
-              d.constructor === Date ? d.setHours(0,0,0,0) :
-              d.constructor === Array ? new Date(d[0],d[1],d[2]).setHours(0,0,0,0) :
-              d.constructor === Number ? new Date(d).setHours(0,0,0,0) :
-              d.constructor === String ? new Date(d).setHours(0,0,0,0) :
-              typeof d === "object" ? new Date(d.year,d.month,d.date).setHours(0,0,0,0) :
-              NaN
-          );
-      },
-      compare:function(a,b) {
-          // Compare two dates (could be of any type supported by the convert
-          // function above) and returns:
-          //  -1 : if a < b
-          //   0 : if a = b
-          //   1 : if a > b
-          // NaN : if a or b is an illegal date
-          // NOTE: The code inside isFinite does an assignment (=).
-        return (
-            isFinite(a=this.convert(a).valueOf()) &&
-            isFinite(b=this.convert(b).valueOf()) ?
-            (a>b)-(a<b) :
-            NaN
-        );
-      },
-      inRange:function(d,start,end) {
-          // Checks if date in d is between dates in start and end.
-          // Returns a boolean or NaN:
-          //    true  : if d is between start and end (inclusive)
-          //    false : if d is before start or after end
-          //    NaN   : if one or more of the dates is illegal.
-          // NOTE: The code inside isFinite does an assignment (=).
-         return (
-              isFinite(d=this.convert(d).valueOf()) &&
-              isFinite(start=this.convert(start).valueOf()) &&
-              isFinite(end=this.convert(end).valueOf()) ?
-              start <= d && d <= end :
-              NaN
-          );
-      }
-  };
-
-  /*
-
-  CONTROLLER CODE
-
-  */
-
-
+App.controller('Ctrl', function ($scope, $http, $timeout){
   $scope.loginUsername = "";
   $scope.loginPassword = "";
   $scope.userName="";
@@ -78,9 +13,12 @@ App.controller('Ctrl', function ($scope, $http){
   $scope.dailyResults = [];
   $scope.resultsDisplayed = false;
 
+  //RealTime
+  $scope.realTimeActive = false;
+
   //Page switching
-  $scope.resultsVisible = true;
-  $scope.summaryVisible = false;
+  $scope.resultsVisible = false;
+  $scope.summaryVisible = true;
   $scope.realtimeVisible = false;
 
 
@@ -102,23 +40,25 @@ App.controller('Ctrl', function ($scope, $http){
   $scope.realtimeVisible = true;
 };
 
-  $scope.logIn = function() {
-    var url = "https://api.mongolab.com/api/1/databases/activity_recognition/collections/users?q={%22username%22:%20%22" + $scope.loginUsername + "%22,%22password%22:%22" + $scope.loginPassword + "%22}&apiKey=" + $scope.apiKey;
-    $http.get(url).success(
-      function(data, status, headers, config) {
-        if(data.length==1){
-          var user = data[0];
-          $scope.loggedIn=true;
-          $scope.userName=user.name;
-          $scope.userSurname=user.surname;
-          $scope.userId=user.userid;
-          $scope.loadResults();
-        }
-      }
-      );
-  };
 
-  $scope.results = [
+
+$scope.logIn = function() {
+  var url = "https://api.mongolab.com/api/1/databases/activity_recognition/collections/users?q={%22username%22:%20%22" + $scope.loginUsername + "%22,%22password%22:%22" + $scope.loginPassword + "%22}&apiKey=" + $scope.apiKey;
+  $http.get(url).success(
+    function(data, status, headers, config) {
+      if(data.length==1){
+        var user = data[0];
+        $scope.loggedIn=true;
+        $scope.userName=user.name;
+        $scope.userSurname=user.surname;
+        $scope.userId=user.userid;
+        $scope.loadResults();
+      }
+    }
+    );
+};
+
+$scope.results = [
   // {date: "May 31, 2013 5:51:45 PM",
   // p: [
   // -848.5727069759712,
@@ -132,7 +72,24 @@ App.controller('Ctrl', function ($scope, $http){
   // }
   ];
 
-$scope.countToTime = function(d) {
+  $scope.loadRealTimeResult = function(){
+    alert("ping");
+    mytimeout = $timeout($scope.loadRealTimeResult,3000);
+  };
+
+  var mytimeout = null;
+
+  $scope.toggleRealTime = function(){
+    if($scope.realTimeActive===true){
+      $scope.realTimeActive=false;
+      $timeout.cancel(mytimeout);
+    }else{
+      $scope.realTimeActive=true;
+      mytimeout = $timeout($scope.loadRealTimeResult,3000);
+    }
+  };
+
+  $scope.countToTime = function(d) {
     var h, m, s;
     d = d * 5;
     d = Number(d) || 0;
@@ -140,116 +97,187 @@ $scope.countToTime = function(d) {
     m = Math.floor(d % 3600 / 60);
     s = Math.floor(d % 3600 % 60);
     return (h > 0 ? h + ":" : "") + (m > 0 ? (m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s;
-};
+  };
 
 
-$scope.calculateDailyTypeDistribution = function (){
-  var dailyResultsArray=[];
-  for (var i = 0; i < $scope.results.length; i++) {
-    if(dates.compare($scope.results[i].date,$scope.pickedDate)===0){
-      dailyResultsArray.push($scope.results[i]);
+
+  $scope.toggleText = function(){
+    if($scope.realTimeActive===true){
+      return "Stop";
+    }else{
+      return "Start";
     }
-  }
-  var tempTypeDistribution = [];
-  for (var j = 0; j < 7; j++) {
-    tempTypeDistribution.push($scope.typeCount(j,dailyResultsArray));
-  }
-  $scope.typeDistribution=tempTypeDistribution;
-  var sum = 0;
-  for(var k=0;k<tempTypeDistribution.length;k++){
-    sum = sum + tempTypeDistribution[k];
-  }
-  if(sum>0){
-    $scope.resultsDisplayed=true;
-  }else{
-    $scope.resultsDisplayed=false;
-  }
-};
+  };
 
-
-$scope.updateDateFunction = function (date){
-  $scope.pickedDate = date;
-  $scope.calculateDailyTypeDistribution();
-};
-
-$scope.loadResults = function() {
-  var url = "https://api.mongolab.com/api/1/databases/activity_recognition/collections/classification_results?q={%22userid%22:"+$scope.userId+ "}&apiKey=" + $scope.apiKey;
-  $http.get(url).success(
-    function(data, status, headers, config) {
-      for (var i = 0; i < data.length; i++) {
-        $scope.results.push({date: data[i].date, p: data[i].p, result: data[i].result});
+  $scope.calculateDailyTypeDistribution = function (){
+    var dailyResultsArray=[];
+    for (var i = 0; i < $scope.results.length; i++) {
+      if(dates.compare($scope.results[i].date,$scope.pickedDate)===0){
+        dailyResultsArray.push($scope.results[i]);
       }
     }
-
-    );
-};
-
-$scope.typeCount = function(type,array){
-  var count = 0;
-  for (var i = 0; i < array.length; i++) {
-    if(array[i].result==type){
-      count++;
+    var tempTypeDistribution = [];
+    for (var j = 0; j < 7; j++) {
+      tempTypeDistribution.push($scope.typeCount(j,dailyResultsArray));
     }
-  }
-  return count;
-};
+    $scope.typeDistribution=tempTypeDistribution;
+    var sum = 0;
+    for(var k=0;k<tempTypeDistribution.length;k++){
+      sum = sum + tempTypeDistribution[k];
+    }
+    if(sum>0){
+      $scope.resultsDisplayed=true;
+    }else{
+      $scope.resultsDisplayed=false;
+    }
+  };
 
-$scope.typeToString = function(type){
-  switch (type) {
+
+  $scope.updateDateFunction = function (date){
+    $scope.pickedDate = date;
+    $scope.calculateDailyTypeDistribution();
+  };
+
+  $scope.loadResults = function() {
+    var url = "https://api.mongolab.com/api/1/databases/activity_recognition/collections/classification_results?q={%22userid%22:"+$scope.userId+ "}&apiKey=" + $scope.apiKey;
+    $http.get(url).success(
+      function(data, status, headers, config) {
+        for (var i = 0; i < data.length; i++) {
+          $scope.results.push({date: data[i].date, p: data[i].p, result: data[i].result});
+        }
+      }
+
+      );
+  };
+
+  $scope.typeCount = function(type,array){
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+      if(array[i].result==type){
+        count++;
+      }
+    }
+    return count;
+  };
+
+  $scope.typeToString = function(type){
+    switch (type) {
       case 0:
-        return "Walking (0)";
+      return "Walking (0)";
       case 1:
-        return "Fast Walking (1)";
+      return "Fast Walking (1)";
       case 2:
-        return "Walking up the stairs (2)";
+      return "Walking up the stairs (2)";
       case 3:
-        return "Walking down the stairs (3)";
+      return "Walking down the stairs (3)";
       case 4:
-        return "Sitting (4)";
+      return "Sitting (4)";
       case 5:
-        return "Standing up (5)";
+      return "Standing up (5)";
       case 6:
-        return "Jumping (6)";
+      return "Jumping (6)";
       case 7:
-        return "Test: Wave Sideways (7)";
+      return "Test: Wave Sideways (7)";
       case 8:
-        return "Test: Wave Forward (8)";
+      return "Test: Wave Forward (8)";
       case 9:
-        return "Test: Unidentified (9)";
+      return "Test: Unidentified (9)";
       default:
-        return "Unspecified";
+      return "Unspecified";
 
-      }
-};
-
-$scope.totalTypeCount = function(type){
-  var count = 0;
-  for (var i = 0; i < $scope.results.length; i++) {
-    if($scope.results[i].result==type){
-      count++;
     }
-  }
-  return count;
-};
+  };
+
+  $scope.totalTypeCount = function(type){
+    var count = 0;
+    for (var i = 0; i < $scope.results.length; i++) {
+      if($scope.results[i].result==type){
+        count++;
+      }
+    }
+    return count;
+  };
 
 
-$scope.todos = [{text:'Learn AngularJS', done: false},{text: 'Build an app', done: false}];
+  $scope.todos = [{text:'Learn AngularJS', done: false},{text: 'Build an app', done: false}];
 
-$scope.totalTodos = $scope.todos.length;
+  $scope.totalTodos = $scope.todos.length;
 
 
-$scope.getTotalTodos = function() {
-  return $scope.todos.length;
-};
+  $scope.getTotalTodos = function() {
+    return $scope.todos.length;
+  };
 
-$scope.addTodo = function() {
-  $scope.todos.push({text:$scope.formTodoText, done: false});
-  $scope.formTodoText='';
-};
+  $scope.addTodo = function() {
+    $scope.todos.push({text:$scope.formTodoText, done: false});
+    $scope.formTodoText='';
+  };
 
-$scope.clearCompleted = function(){
-  $scope.todos = _.filter($scope.todos, function(todo){
-    return !todo.done;
+  $scope.clearCompleted = function(){
+    $scope.todos = _.filter($scope.todos, function(todo){
+      return !todo.done;
+    });
+  };
+
+
+
+/*
+
+DATE MANIPULATION UTILITY FUNCTIONS
+
+*/
+var dates = {
+  convert:function(d) {
+        // Converts the date in d to a date-object. The input can be:
+        //   a date object: returned without modification
+        //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+        //   a number     : Interpreted as number of milliseconds
+        //                  since 1 Jan 1970 (a timestamp)
+        //   a string     : Any format supported by the javascript engine, like
+        //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+        //  an object     : Interpreted as an object with year, month and date
+        //                  attributes.  **NOTE** month is 0-11.
+        return (
+          d.constructor === Date ? d.setHours(0,0,0,0) :
+          d.constructor === Array ? new Date(d[0],d[1],d[2]).setHours(0,0,0,0) :
+          d.constructor === Number ? new Date(d).setHours(0,0,0,0) :
+          d.constructor === String ? new Date(d).setHours(0,0,0,0) :
+          typeof d === "object" ? new Date(d.year,d.month,d.date).setHours(0,0,0,0) :
+          NaN
+          );
+      },
+      compare:function(a,b) {
+        // Compare two dates (could be of any type supported by the convert
+        // function above) and returns:
+        //  -1 : if a < b
+        //   0 : if a = b
+        //   1 : if a > b
+        // NaN : if a or b is an illegal date
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+          isFinite(a=this.convert(a).valueOf()) &&
+          isFinite(b=this.convert(b).valueOf()) ?
+          (a>b)-(a<b) :
+          NaN
+          );
+      },
+      inRange:function(d,start,end) {
+        // Checks if date in d is between dates in start and end.
+        // Returns a boolean or NaN:
+        //    true  : if d is between start and end (inclusive)
+        //    false : if d is before start or after end
+        //    NaN   : if one or more of the dates is illegal.
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+          isFinite(d=this.convert(d).valueOf()) &&
+          isFinite(start=this.convert(start).valueOf()) &&
+          isFinite(end=this.convert(end).valueOf()) ?
+          start <= d && d <= end :
+          NaN
+          );
+      }
+    };
+
+
+
   });
-};
-});
--- appjs/directives.js
+++ appjs/directives.js
@@ -1,42 +1,51 @@
 App.directive('chart', function(){
-    return{
-        restrict: 'E',
-        link: function(scope, elem, attrs){
-            var data = scope[attrs.ngModel];
-            var r = Raphael("pie");
-            var pie = null;
+  return{
+    restrict: 'E',
+    link: function(scope, elem, attrs){
+      var data = scope[attrs.ngModel];
+      var r = Raphael("pie");
+      var pie = null;
 
-            scope.$watch('typeDistribution', function(v){
-            if(pie){
-                   r.clear();
-                   pie.clear();
-            }
-            var sum = 0;
-            for(var i=0;i<v.length;i++){
-              sum = sum + v[i];
-            }
-            if(sum>0){
-              pie = r.piechart(120, 120, 90, v, { legend: ["%% - Walking (0)", "%% - Fast Walking (1)", "%% - Walking up the stairs (2)", "%% - Walking down the stairs (3)", "%% - Sitting (4)", "%% - Standing (5)", "%% - Jumping (6)"], legendpos: "east" });
-              pie.hover(function () {
-                  this.sector.stop();
-                  this.sector.scale(1.1, 1.1, this.cx, this.cy);
 
-                  if (this.label) {
-                      this.label[0].stop();
-                      this.label[0].attr({ r: 7.5 });
-                      this.label[1].attr({ "font-weight": 800 });
-                  }
-              }, function () {
-                  this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+      // scope.$watch('realTimeActive', function(v){
+      //   if(v){
+      //     mytimeout = $timeout(scope.loadRealTimeResult,3000);
+      //   }else{
+      //     $timeout.cancel(mytimeout);
+      //   }
+      // });
 
-                  if (this.label) {
-                      this.label[0].animate({ r: 5 }, 500, "bounce");
-                      this.label[1].attr({ "font-weight": 400 });
-                  }
-              });
-            }
-              }
-            );
-        }
-    };
+      scope.$watch('typeDistribution', function(v){
+        if(pie){
+         r.clear();
+         pie.clear();
+       }
+       var sum = 0;
+       for(var i=0;i<v.length;i++){
+        sum = sum + v[i];
+      }
+      if(sum>0){
+        pie = r.piechart(120, 120, 90, v, { legend: ["%% - Walking (0)", "%% - Fast Walking (1)", "%% - Walking up the stairs (2)", "%% - Walking down the stairs (3)", "%% - Sitting (4)", "%% - Standing (5)", "%% - Jumping (6)"], legendpos: "east" });
+        pie.hover(function () {
+          this.sector.stop();
+          this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+          if (this.label) {
+            this.label[0].stop();
+            this.label[0].attr({ r: 7.5 });
+            this.label[1].attr({ "font-weight": 800 });
+          }
+        }, function () {
+          this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+
+          if (this.label) {
+            this.label[0].animate({ r: 5 }, 500, "bounce");
+            this.label[1].attr({ "font-weight": 400 });
+          }
+        });
+      }
+    }
+    );
+}
+};
 });
